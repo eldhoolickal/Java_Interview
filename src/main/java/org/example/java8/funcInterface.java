aa003377ceab82be5fbdeb36a931fdd406d7f09d
eldhoolickal <eldhoolickal@gmail.com>
2024-08-18 14:54:26 +0530

    functional interface example

---
+++ src/main/java/org/example/java8/funcInterface.java
@@ -0,0 +1,19 @@
+package org.example.java8;
+
+public class funcInterface {
+
+    @FunctionalInterface
+    public interface ArithmeticOperation{
+        int operation(int op1,int op2);
+
+    }
+
+
+    public static void main(String[] args) {
+        ArithmeticOperation addOperation = Integer::sum;
+        System.out.println(addOperation.operation(1,2));
+
+        ArithmeticOperation subOperation = (op1, op2) -> { return op1 - op2; };
+        System.out.println(subOperation.operation(4,2));
+    }
+}
